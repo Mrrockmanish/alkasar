2978d529bf719dc6ea511bcfe118b2bb17304f72
Mrrockmanish <mrrockmanish@gmail.com>
2020-08-02 01:39:30 +0300

    сделан раздел плюсов и минусов материалов

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,32 @@ $(document).ready(function () {
 //маска телефона
   $('input[name="tel"]').mask("+7(999) 999-9999");
   
+  $('.material').on('click', '.material__title', function (){
+    $(this).next('.material__content').fadeToggle();
+    $(this).toggleClass('active');
+  });
+
+  const tabsSwitcher = (tabsContainer, tabLink, tabPanel) => {
 
 
+    $(tabsContainer).on('click', `${tabLink}:not(.active)`, function (){
+      const activeTab = $(tabsContainer).find(`${tabLink}.active`);
+      const activePanel = $(tabsContainer).find(`${tabPanel}.active`);
 
 
+      const thisData =  $(this).data('tab');
+
+      activeTab.removeClass('active');
+      $(this).addClass('active');
+
+      activePanel.removeClass('active');
+      $(`[data-tabContent=${thisData}]`).addClass('active');
+
+    });
+  };
 
 
-
+  tabsSwitcher('.materials__wrap', '.materials__tab', '.material');
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       'sm': '480px',
       'md': '768px',
       'lg': '992px',
-      'xl': '1200px',
+      'xl': '1220px',
 
     },
 
