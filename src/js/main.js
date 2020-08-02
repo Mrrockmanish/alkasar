183496019c6b96d06d541640c9139b8231339f30
Mrrockmanish <mrrockmanish@gmail.com>
2020-08-02 04:00:51 +0300

    сделан раздел "реализованные проекты"

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 
 //маска телефона
   $('input[name="tel"]').mask("+7(999) 999-9999");
-  
-  $('.material').on('click', '.material__title', function (){
+
+  $('.material').on('click', '.material__title', function () {
     $(this).next('.material__content').fadeToggle();
     $(this).toggleClass('active');
   });
@@ -11,36 +11,44 @@ $(document).ready(function () {
   const tabsSwitcher = (tabsContainer, tabLink, tabPanel) => {
 
 
-    $(tabsContainer).on('click', `${tabLink}:not(.active)`, function (){
+    $(tabsContainer).on('click', `${tabLink}:not(.active)`, function () {
       const activeTab = $(tabsContainer).find(`${tabLink}.active`);
       const activePanel = $(tabsContainer).find(`${tabPanel}.active`);
-
-
-      const thisData =  $(this).data('tab');
+      const thisData = $(this).data('tab');
 
       activeTab.removeClass('active');
       $(this).addClass('active');
 
       activePanel.removeClass('active');
       $(`[data-tabContent=${thisData}]`).addClass('active');
-
     });
   };
 
-
   tabsSwitcher('.materials__wrap', '.materials__tab', '.material');
 
+  $('.projects-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    appendArrows: $('.project-slider-arrows'),
+    prevArrow: '<div class="project-slider-arrow project-slider-prev bg-light-green flex justify-center items-center"><svg enable-background="new 0 0 551.13 551.13" height="22" viewBox="0 0 551.13 551.13" width="22" xmlns="http://www.w3.org/2000/svg"><path fill="white" d="m189.451 275.565 223.897-223.897v-51.668l-275.565 275.565 275.565 275.565v-51.668z"/></svg></div>',
+    nextArrow: '<div class="project-slider-arrow project-slider-prev bg-light-green flex justify-center items-center"><svg enable-background="new 0 0 551.13 551.13" height="22" viewBox="0 0 551.13 551.13" width="22" xmlns="http://www.w3.org/2000/svg"><path fill="white" d="m361.679 275.565-223.896 223.897v51.668l275.565-275.565-275.565-275.565v51.668z"/></svg></div>',
 
-
-
-
-
-
-
-
-
-
-
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
 
 
 });
