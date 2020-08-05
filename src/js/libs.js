02cc04d9c6a29cdbf0c0f72e18faa413f7f6aa4a
Mrrockmanish <mrrockmanish@gmail.com>
2020-08-05 10:18:47 +0300

    сделана галерея на странице project-page.html

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,8 +1,8 @@
 //= ../../node_modules/jquery/dist/jquery.js
 //= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
 //= ../../node_modules/slick-carousel/slick/slick.js
+//= ../../node_modules/fotorama/fotorama.js
 
-// ../../bower_components/arcticModal/arcticmodal/jquery.arcticmodal.js
 
 
 
